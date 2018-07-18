9955a2c52702f2e9bf41874bb61585e61c8c7d35
guozhongyi <2353976000@qq.com>
2018-07-18 16:17:03 +0800

    第一次提交

---
+++ src/com/accp/Demo.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.accp;
+
+/**
+ * @ClassName:Demo
+ * @Description:TODO{描述类的工能}
+ * @author:gzy
+ * @date:2018年7月18日下午4:10:12
+ */
+public class Demo {
+	public static void main(String[] args) {
+		System.out.println("55555555555555555");
+	}
+}
